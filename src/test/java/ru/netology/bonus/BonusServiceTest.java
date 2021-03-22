98f4210452308a15b769849176ecb03e404bc859
Sergei Lychagin <lychagin0@gmail.com>
2021-03-22 18:45:04 +0400

    создание репозитория

---
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -0,0 +1,22 @@
+package ru.netology.bonus;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    // @org.junit.jupiter.api.Test
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", delimiter = ';')
+    void shouldCalculate(String test, long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+}
